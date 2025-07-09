76b4b8f705099a1b6ee3e2fa784965b04e522ba5
Aldito8 <alditositompul198@gmail.com>
2025-07-09 13:21:53 +0700

    checkpoint1

--- day-3 checkpoint 1/routing-test/src/pages/PostDetail.tsx
+++ day-3 checkpoint 1/routing-test/src/pages/PostDetail.tsx
@@ -1,12 +1,20 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useParams } from "react-router-dom";
 
 export default function PostDetail() {
     const { postId } = useParams();
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold text-primary">Post Detail {postId}</h1>
-            <p className="text-gray-600 mt-2">{postId}</p>
+        <div className="p-4 mt-10">
+            <h1 className="text-2xl font-bold text-primary mb-10">Post Detail</h1>
+            <Card className="w-100">
+                <CardHeader>
+                    <h1 className="text-2xl font-bold text-primary">Post Detail {postId}</h1>
+                </CardHeader>
+                <CardContent className="p-4">
+                    <p className="text-sm text-gray-600">Post id : {postId}</p>
+                </CardContent>
+            </Card>
         </div>
     );
 }
